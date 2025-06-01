5ce8eb26150ef311cfd5002bcea49b92130f5533
MiKPo4eLiK <stassojcuk@gmail.com>
2025-06-01 15:04:39 +0300

    welll

--- app/main.py
+++ app/main.py
@@ -1,15 +1,16 @@
 from typing import Callable
 
-
 def cache(func: Callable) -> Callable:
     result = {}
 
     def wrapper(*args, **kwargs) -> Callable:
-        key_result(args, tuple(kwargs.items()), func.__name__)
-        if key_result in result.keys():
+        key_result = (args, tuple(kwargs.items()), func.__name__)
+        if key_result in result:
             print("Getting from cache")
         else:
             result[key_result] = func(*args, **kwargs)
             print("Calculating new result")
         return result[key_result]
+
     return wrapper
+
